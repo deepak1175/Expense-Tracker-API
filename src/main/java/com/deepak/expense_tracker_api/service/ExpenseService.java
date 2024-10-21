b0a2f1343eacdd9fae4ed782a3eed3fbfc5a9212
abhayrajnegi0416 <abhayrajnegi007@gmail.com>
2024-10-22 00:14:39 +0530

    Initial commit

--- src/main/java/com/deepak/expense_tracker_api/service/ExpenseService.java
+++ src/main/java/com/deepak/expense_tracker_api/service/ExpenseService.java
@@ -11,12 +11,21 @@ import com.deepak.expense_tracker_api.entity.Expense;
 
 public interface ExpenseService {
 	List<ExpenseDTO> getallexpenses(Pageable page);
+	
 	ExpenseDTO getexpensebyid(String id);
+	
 	void deletebyid(String id);
+	
 	ExpenseDTO saveexpensedetails(ExpenseDTO expensedto);
+	
 	ExpenseDTO updateexpensedetails(String id,ExpenseDTO expense);
+	
+	
 	List<ExpenseDTO> readbycategory(String category,Pageable page);
+	
 	List<ExpenseDTO> readbyname(String name,Pageable page);
+	
 	List<ExpenseDTO> readbydate(Date startdate,Date enddate,Pageable page);
+	
 	void deletebyuserid();
 }
